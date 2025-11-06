43af3e16e55026334b48091bfeabfe46acc905e3
Clément Dubois <=contact.clementdbs@gmail.com>
2025-11-06 14:24:46 +0100

    get movie by genre

--- read/read_data.py
+++ read/read_data.py
@@ -20,6 +20,21 @@ def get_movie_by_age_limit(age_limit:int):
                 list_movies.append(row)
         return list_movies
 
+# Récupère une liste de films par genre
+def get_movie_by_genre(genre:str):
+    with open("write/data/movies.csv", "r", newline='', encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        list_movies = []
+        for row in reader:
+            print(row[0].split(',')[3])
+            if row[0].split(',')[3] == genre:
+                list_movies.append(row)
+        return list_movies
+    
+
+
+
 if __name__ == "__main__":
     print(get_movie_by_name("La La Land"))
     print(get_movie_by_age_limit(15))
+    print(get_movie_by_genre("Action"))
