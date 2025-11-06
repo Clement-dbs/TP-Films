632c3e7756e7b3c906f75487d2fcb56ed7e275f0
Clément Dubois <=contact.clementdbs@gmail.com>
2025-11-06 14:28:35 +0100

    get movie by year

--- read/read_data.py
+++ read/read_data.py
@@ -31,6 +31,17 @@ def get_movie_by_genre(genre:str):
                 list_movies.append(row)
         return list_movies
     
+# Récupère une liste de films par annee
+def get_movie_by_year(annee1:int, annee2:int):
+    with open("write/data/movies.csv", "r", newline='', encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        list_movies = []
+        for row in reader:
+            print(row[0].split(',')[2])
+            if int(row[0].split(',')[2]) >= annee1 and int(row[0].split(',')[2]) <= annee2:
+                list_movies.append(row)
+        return list_movies
+    
 
 
 
@@ -38,3 +49,4 @@ if __name__ == "__main__":
     print(get_movie_by_name("La La Land"))
     print(get_movie_by_age_limit(15))
     print(get_movie_by_genre("Action"))
+    print(get_movie_by_year(1995,2010))
