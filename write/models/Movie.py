f24e998a8b24b48eef81c4bdeb46db27e01cf83b
Clément Dubois <=contact.clementdbs@gmail.com>
2025-11-06 09:17:07 +0100

    commit base project

---
+++ write/models/Movie.py
@@ -0,0 +1,15 @@
+class Movie():
+    id = 31
+    def __init__(self, titre:str, anne_production:int,genre:str,age_limite:int):
+        id += 1
+        self.titre = titre
+        self.annee_production = anne_production
+        self.genre = genre
+        self.age_limite = age_limite
+    
+    def __str__(self):
+        print(f"==={self.titre}===\n")
+        print(f"Année de production : {self.annee_production}\n")
+        print(f"Genre : {self.genre}\n")
+        print(f"Age limite : {self.age_limite}\n")
+
